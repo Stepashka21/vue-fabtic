3edb99d365c94957b8d64bdd3bb8a51d85a98c80
Stepashka_21 <stepanesse@gmail.com>
2024-04-08 22:19:19 +0400

    Сохраниение слоев и открытие проекта с отображением существующих слоев в нем

--- src/components/ProjectEditor.vue
+++ src/components/ProjectEditor.vue
@@ -2,14 +2,17 @@
   <div>
     <h1>Редактор проекта "{{ projectName }}"</h1>
     <!-- <router-link :to="{ name: 'ReplaceDialog' }">Перейти на страницу Replace</router-link> -->
-    <div style="display: flex;flex-direction: row;">
+    <div style="display: flex; flex-direction: row">
       <div class="lay">
         <h2>Layers</h2>
-          <draggable v-model='layers' @end='above'>
-            <div v-for='(layer, index) in layers' :key='index'> 
-              <span @click="selectLayer(layer)" class="layer">{{layer.name}} / {{ layer.selected ? "selected" : "deselected" }}</span>
-            </div>
-          </draggable>
+        <draggable v-model="layers" @end="above">
+          <div v-for="(layer, index) in layers" :key="index">
+            <span @click="selectLayer(layer)" class="layer"
+              >{{ layer.name }} /
+              {{ layer.selected ? "selected" : "deselected" }}</span
+            >
+          </div>
+        </draggable>
       </div>
 
       <div>
@@ -17,27 +20,43 @@
           <button class="btns" @click="addRectangle">Квадрат</button>
           <button class="btns" @click="addCircle">Кругг</button>
           <button class="btns" @click="addImg">Картинка</button>
+          <button class="btns" @click="addText">Текст</button>
+          <select v-model="selectedFont" @change="applyFont">
+            <option v-for="font in fonts" :key="font" :value="font">
+              {{ font }}
+            </option>
+          </select>
+
           <button class="btns" @click="saveProject">Сохранить</button>
-          <button class="btns" @click="saveCanvasAsImage">Сохранить как картинку</button>
+          <button class="btns" @click="saveCanvasAsImage">
+            Сохранить как картинку
+          </button>
           <button class="btns" @click="deleteEl">Удалить элемент</button>
           <button class="btns" @click="clearCanvas">Очистить холст</button>
         </div>
 
-        <div class="canav">      
-          <canvas ref="canvas" width="1000" height="600" @click="deselectAll"></canvas>
+        <div class="canav">
+          <canvas
+            ref="canvas"
+            width="1000"
+            height="600"
+            @click="deselectAll"
+          ></canvas>
         </div>
       </div>
     </div>
   </div>
 </template>
-
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.6.0/fabric.min.js"></script>
+  
 <script>
 import { fabric } from "fabric";
-import draggable from 'vuedraggable'
+import draggable from "vuedraggable";
 
 export default {
   name: "ProjectEditor",
-  
+
   components: {
     draggable,
   },
@@ -45,13 +64,22 @@ export default {
   data() {
     return {
       canvas: null,
-      
+
       projectName: "",
       projectFilePath: "",
       projectData: null,
-      
+
       layers: [],
       selectedLayer: null,
+
+      fonts: [
+        "Times New Roman",
+        "Pacifico",
+        "VT323",
+        "Quicksand",
+        "Inconsolata",
+      ],
+      selectedFont: "Times New Roman",
     };
   },
 
@@ -60,7 +88,16 @@ export default {
       isDrawingMode: false,
     });
     this.loadProject();
-    this.canvas.on('object:selected', console.log)
+    // this.initializeCanvas();
+    this.canvas.on("selection:created", (e) => {
+      const activeObject = e.target;
+      const selectedLayer = this.layers.find(
+        (layer) => layer.object === activeObject
+      );
+      if (selectedLayer) {
+        selectedLayer.selected = true;
+      }
+    });
   },
 
   methods: {
@@ -70,20 +107,20 @@ export default {
       this.projectName = this.$route.params.projectName;
       // Формируем путь к файлу проекта
       this.projectFilePath = `projects/${this.projectName}.json`;
-      console.log(this.projectFilePath);
 
       // Загружаем файл проекта с помощью fetch
       fetch(this.projectFilePath)
         .then((response) => {
           if (!response.ok) {
-            throw new Error("файл не найден");
+            throw new Error("Файл не найден");
           }
           return response.json();
         })
         .then((data) => {
           // Сохраняем данные проекта
+          console.log(data);
           this.projectData = data;
-          // Восстанавливаем состояние холста из данных проекта
+          // Восстанавливаем состояние холста и слои из данных проекта
           this.restoreCanvasState();
         })
         .catch((error) => {
@@ -94,24 +131,45 @@ export default {
     // Восстанавливаем состояние холста из данных проекта
     restoreCanvasState() {
       if (!this.projectData) return;
-      // Восстанавливаем состояние холста из данных проекта
-      this.canvas.loadFromJSON(this.projectData, () => {
+
+      this.canvas.loadFromJSON(this.projectData.canvas, () => {
         this.canvas.renderAll();
       });
+      // Восстановление слоев
+      setTimeout(() => {
+        if (this.projectData.layers && Array.isArray(this.projectData.layers)) {
+          this.projectData.layers.forEach((layerData) => {
+            console.log(layerData);
+            // Создаем объект слоя на основе данных из файла JSON
+            const object = this.canvas.item(layerData.index);
+            object.type = layerData.name;
+            object.selected = false;
+            if (object) {
+              // Добавляем объект на холст и в список слоев
+              this.addLayer(object);
+            }
+          });
+        }
+      }, 0);
+      // Восстанавливаем состояние холста
     },
 
     above(ev) {
-      this.layers[ev.newIndex].object.moveTo(this.layers.length - ev.newIndex - 1)
+      this.layers[ev.newIndex].object.moveTo(
+        this.layers.length - ev.newIndex - 1
+      );
       // console.log(ev)
     },
     addRectangle() {
       const rect = new fabric.Rect({
         width: 100,
         height: 100,
-        fill: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`,
+        fill: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+          Math.random() * 256
+        )}, ${Math.floor(Math.random() * 256)})`,
         left: 10,
         top: 10,
-        selectable: true
+        selectable: true,
       });
       this.canvas.add(rect);
       this.addLayer(rect);
@@ -119,10 +177,12 @@ export default {
     addCircle() {
       const circle = new fabric.Circle({
         radius: 50,
-        fill: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`,
+        fill: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+          Math.random() * 256
+        )}, ${Math.floor(Math.random() * 256)})`,
         left: 50,
         top: 50,
-        selectable: true
+        selectable: true,
       });
       this.canvas.add(circle);
       this.addLayer(circle);
@@ -150,18 +210,18 @@ export default {
 
     addListeners(layer) {
       layer.object.onSelect = () => {
-        layer.selected = true
-      }
+        layer.selected = true;
+      };
       layer.object.onDeselect = () => {
-        layer.selected = false
-      }
+        layer.selected = false;
+      };
     },
     addLayer(object) {
-      const layer = { name: object.type, object, selected: false }
+      const layer = { name: object.type, object, selected: false };
       this.layers.unshift(layer);
-      this.addListeners(layer)
+      this.addListeners(layer);
     },
-     
+
     deselectAll() {
       this.canvas.discardActiveObject();
       this.selectedLayer = null;
@@ -178,28 +238,36 @@ export default {
 
     saveProject() {
       // Получаем JSON-представление состояния холста
-      const jsonData = JSON.stringify(this.canvas.toJSON());
+      const canvasData = this.canvas.toJSON();
 
-        // Создаем Blob из JSON-данных
-        const blob = new Blob([jsonData], { type: "application/json" });
+      // Получаем информацию о слоях
+      const layersData = this.layers.map((layer) => ({
+        name: layer.name,
+        index: this.canvas.getObjects().indexOf(layer.object),
+      }));
 
-        // Создаем объект URL для Blob
-        const url = URL.createObjectURL(blob);
+      // Создаем объект с информацией о слоях и состоянии холста
+      const projectData = {
+        canvas: canvasData,
+        layers: layersData,
+      };
 
-        // Создаем ссылку для загрузки
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = this.projectName + ".json";
+      // Преобразуем данные в JSON
+      const jsonData = JSON.stringify(projectData);
 
-        // Добавляем ссылку в документ
-        document.body.appendChild(link);
+      // Создаем Blob из JSON-данных
+      const blob = new Blob([jsonData], { type: "application/json" });
 
-        // Эмулируем клик для загрузки файла
-        link.click();
+      // Создаем ссылку для загрузки
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = this.projectName + ".json";
 
-        // Удаляем ссылку из документа и освобождаем ресурсы
-        document.body.removeChild(link);
-        URL.revokeObjectURL(url);
+      // Эмулируем клик для загрузки файла
+      link.click();
+
+      // Освобождаем ресурсы
+      URL.revokeObjectURL(link.href);
     },
 
     saveCanvasAsImage() {
@@ -223,15 +291,41 @@ export default {
     },
 
     deleteEl() {
-      const activeObject = this.canvas.getActiveObject()
-      this.layers = this.layers.filter(layer => layer.object !== activeObject)
+      const activeObject = this.canvas.getActiveObject();
+      this.layers = this.layers.filter(
+        (layer) => layer.object !== activeObject
+      );
       this.canvas.remove(activeObject);
     },
 
     clearCanvas() {
-      this.canvas.clear(); 
-      this.layers = []; 
-    }
+      this.canvas.clear();
+      this.layers = [];
+    },
+
+    addText() {
+      const text = new fabric.Textbox("Lorum ipsum dolor sit amet", {
+        left: 50,
+        top: 50,
+        width: 150,
+        fill: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+          Math.random() * 256
+        )}, ${Math.floor(Math.random() * 256)})`,
+        fontSize: 20,
+        fontFamily: this.selectedFont, // применяем выбранный шрифт
+      });
+      this.canvas.add(text);
+      this.addLayer(text);
+    },
+
+    applyFont() {
+      if (this.selectedFont !== "Times New Roman") {
+        this.loadAndUse(this.selectedFont);
+      } else {
+        this.canvas.getActiveObject().set("fontFamily", this.selectedFont);
+        this.canvas.requestRenderAll();
+      }
+    },
   },
 };
 </script>
@@ -241,7 +335,7 @@ body {
   display: flex;
   background-color: white;
 }
-.layer{
+.layer {
   cursor: pointer;
   margin: 10px;
   background-color: #b1b1b1;
@@ -257,12 +351,17 @@ canvas {
 .buttunsGrid {
   display: flex;
   flex-direction: row;
-  justify-content: flex-start;
-  max-width: 50vw;
+  justify-content: space-between;
+  max-width: 71vw;
+  height: 50px;
 }
 
 .btns {
   font-size: 16px;
   max-width: fit-content;
+}
+select {
+  max-height: 20px;
+  margin-top: 22px;
 }
 </style>
